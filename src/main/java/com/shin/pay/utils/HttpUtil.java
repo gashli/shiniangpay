ad74c5bb6c16ef3d5acffda45e18f49de20ac61a
gashli <gashli@163.com>
2015-11-02 00:10:37 +0800

    #add method

--- src/main/java/com/shin/pay/utils/HttpUtil.java
+++ src/main/java/com/shin/pay/utils/HttpUtil.java
@@ -1,29 +1,7 @@
 package com.shin.pay.utils;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import net.sf.json.JSONObject;
-import org.apache.http.*;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.net.URL;
-import java.net.URLDecoder;
 import java.security.SecureRandom;
 import java.util.Iterator;
 import java.util.List;
@@ -31,14 +9,34 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 /**
  * @Author gaoshiliang
  * @Date 15/10/23.
  */
 public class HttpUtil {
 
-    private static Logger logger = LoggerFactory.getLogger(HttpUtil.class);    //日志记录
-
+    private static Logger logger = LoggerFactory.getLogger(HttpUtil.class); // 日志记录
 
     /**
      * 获取用户ip
@@ -72,7 +70,6 @@ public class HttpUtil {
         return userInfoMap;
     }
 
-
     public static String doPost(String requestUrl, Map<String, String> paramMap) {
         String responseContent = null;
         if (requestUrl.startsWith("https://")) {
@@ -90,16 +87,16 @@ public class HttpUtil {
         BufferedReader bufferedReader = null;
         try {
             SSLContext context = SSLContext.getInstance("SSL");
-            context.init(null, new TrustManager[]{new TrustAnyTrustManager()}, new SecureRandom());
+            context.init(null, new TrustManager[] { new TrustAnyTrustManager() }, new SecureRandom());
             URL url = new URL(requestUrl);
 
             httpsURLConnection = (HttpsURLConnection) url.openConnection();
             httpsURLConnection.setRequestMethod("POST");
             httpsURLConnection.setSSLSocketFactory(context.getSocketFactory());
             httpsURLConnection.setHostnameVerifier(new TrustAnyHostnameVerifier());
-            //true表示建立连接后，需要从向连接输入数据
+            // true表示建立连接后，需要从向连接输入数据
             httpsURLConnection.setDoInput(true);
-            //true表示需要向连接读取数据
+            // true表示需要向连接读取数据
             httpsURLConnection.setDoOutput(true);
             httpsURLConnection.connect();
 
@@ -147,11 +144,11 @@ public class HttpUtil {
             return "";
         }
         String responseContent = "";
-        //建立http连接
+        // 建立http连接
         HttpClient httpClient = new DefaultHttpClient();
-        //使用post方式
+        // 使用post方式
         HttpPost httpPost = new HttpPost(requestUrl);
-        //post参数
+        // post参数
         List<NameValuePair> nameValuePairs = Lists.newArrayList();
         Iterator<String> iterator = paramMap.keySet().iterator();
         InputStream inputStream = null;
@@ -164,27 +161,27 @@ public class HttpUtil {
             httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
 
             logger.info(httpPost.toString());
-            //获取返回结果
+            // 获取返回结果
             HttpResponse response = httpClient.execute(httpPost);
 
-            //获取返回实体
+            // 获取返回实体
             HttpEntity httpEntity = response.getEntity();
 
-            //获取头文信息
+            // 获取头文信息
             Header header = httpEntity.getContentType();
 
             String headString = "";
             if (header != null) {
                 headString = header.getValue();
             }
-            //得到http请求中的编码
+            // 得到http请求中的编码
             String charset = getEncoding(headString);
 
             if (httpEntity != null) {
                 String lineTemp;
                 inputStream = httpEntity.getContent();
                 StringBuffer stringBuffer = new StringBuffer();
-                //读数据流
+                // 读数据流
                 Reader reader = new InputStreamReader(inputStream, "UTF-8");
                 BufferedReader bufferedReader = new BufferedReader(reader);
                 while ((lineTemp = bufferedReader.readLine()) != null) {
@@ -222,9 +219,9 @@ public class HttpUtil {
     }
 
     private static boolean matcher(String header, String pattern) {
-        //匹配模式
+        // 匹配模式
         Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE + Pattern.UNICODE_CASE);
-        //去匹配header中的串
+        // 去匹配header中的串
         Matcher matcher = p.matcher(header);
         if (matcher.find()) {
             return true;
@@ -243,15 +240,15 @@ public class HttpUtil {
         String strResult = "";
         try {
             DefaultHttpClient client = new DefaultHttpClient();
-            //发送get请求
+            // 发送get请求
             HttpGet request = new HttpGet(url);
             HttpResponse response = client.execute(request);
 
-            /**请求发送成功，并得到响应**/
+            /** 请求发送成功，并得到响应 **/
             if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                /**读取服务器返回过来的json字符串数据**/
+                /** 读取服务器返回过来的json字符串数据 **/
                 strResult = EntityUtils.toString(response.getEntity());
-                logger.info("result:"+strResult);
+                logger.info("result:" + strResult);
             } else {
                 logger.error("get请求提交失败:" + url);
             }
